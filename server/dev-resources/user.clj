546f8c7c8e123d0ba2fdf5374a5865702c321aab
Mike Schutte <schutte@clinc.com>
2018-11-21 22:34:04 -0600

    Add users-paths relationship

--- server/dev-resources/user.clj
+++ server/dev-resources/user.clj
@@ -1,10 +1,25 @@
 (ns user
   (:require
     [server.schema :as s]
-    [com.walmartlabs.lacinia :refer [execute]]))
+    [com.walmartlabs.lacinia :refer [execute]]
+    [clojure.walk :as walk])
+  (:import (clojure.lang IPersistentMap)))
+
+(defn simplify
+  "Converts all ordered maps nested within the map into standard hash maps, and
+  sequences into vectors, which makes for easier constants in the tests, and eliminates ordering problems."
+  [m]
+  (walk/postwalk
+    (fn [node]
+      (cond
+        (instance? IPersistentMap node) (into {} node)
+        (seq? node) (vec node)
+        :else node))
+    m))
 
 (def schema (s/load-schema))
 
 (defn q
   [query-string]
-  (execute schema query-string nil nil))
+  (-> (execute schema query-string nil nil)
+      simplify))
--- server/src/server/schema.clj
+++ server/src/server/schema.clj
@@ -6,20 +6,32 @@
     [com.walmartlabs.lacinia.schema :as schema]
     [clojure.edn :as edn]))
 
-(defn paths-resolver [context args value]
-  [{:id 10 :name "Journaling" :description "blah"}
-   {:id 14 :name "Daily run" :description "blahblah"}]
-  )
+(def ^:private read-resource
+  (comp edn/read-string slurp io/resource))
+
+(defn resolve-user-paths
+  [paths context args user]
+  (let [{:keys [user_id]} args
+        {:keys [id]} user]
+    (filter (comp (partial = (Integer. (or user_id id))) :user_id) paths)))
+
+(defn resolve-path-user [users-map c a path]
+  (let [{:keys [user_id]} path]
+      (get users-map user_id {:username "not found"})))
+
+(defn coll-to-map [xs] (reduce #(assoc %1 (:id %2) %2) {} xs))
 
 (defn resolver-map
   []
-  {:query/paths-by-user-id paths-resolver
-   })
+  (let [data (read-resource "blocks-data.edn")
+        paths (:paths data)
+        users-map (->> data :users coll-to-map)]
+    {:query/paths-by-user-id (partial resolve-user-paths paths)
+     :Path/user (partial resolve-path-user users-map)
+     :User/paths (partial resolve-user-paths paths)}))
 
 (defn load-schema
   []
-  (-> (io/resource "blocks-schema.edn")
-      slurp
-      edn/read-string
+  (-> (read-resource "blocks-schema.edn")
       (util/attach-resolvers (resolver-map))
       schema/compile))
